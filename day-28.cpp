3398b2b395cf8c03561d337b38a765729203bc1a
Akshat Khatri <136313166+akshatkhatri@users.noreply.github.com>
2023-12-11 02:09:52 +0530

    Create day-28.cpp

---
+++ day-28.cpp
@@ -0,0 +1,18 @@
+Leetcode DSA 867. Transpose Matrix
+
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int row=matrix.size();
+        int col=matrix[0].size();
+        vector<vector<int>>result(col,vector<int>(row,0));
+        
+        for (int i = 0; i < col; ++i) {
+            for (int j = 0; j < row; ++j) {
+                result[i][j] = matrix[j][i];
+            }
+        }
+    return result;
+    }
+
+};
