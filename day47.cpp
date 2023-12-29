72a3a5941a2a658c12a9ae54bce582112e141a8d
Akshat Khatri <136313166+akshatkhatri@users.noreply.github.com>
2023-12-29 23:46:20 +0530

    Update day47.cpp

--- day47.cpp
+++ day47.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left=0;
+        int right=height.size()-1;
+        int total_water=0;
+        int dist;
+        while(left<right){
 
+            if(height[left]<=height[right]){
+                dist=height[left]*(right-left);
+                left++;
+            }
+            else{
+                dist=height[right]*(right-left);
+                right--;
+            }
+            
+            total_water=max(total_water,dist);
+        }
+        return total_water;
+    }
+};
