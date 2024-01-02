1d327fde45e38714d2b5c472203f8ea7945acf88
Akshat Khatri <136313166+akshatkhatri@users.noreply.github.com>
2024-01-02 14:27:37 +0530

    Update day50(1).cpp

--- day50(1).cpp
+++ day50(1).cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         int i=0;
+        // int big_int_handler=nums.size()-1;
 
              while(i<nums.size()){
-                    if(nums[i]>0 && nums[i]<=nums.size()-1 && nums[nums[i]-1]!=i+1){
+                    if(nums[i]>0 && nums[i]<=nums.size()-1 && nums[nums[i]-1]!=nums[i]){
                     swap(nums[i],nums[nums[i]-1]);    
                     }
 
