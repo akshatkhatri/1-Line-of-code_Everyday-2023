4d2accf27bc845ebb4d39bf661132aff43828ba4
Akshat Khatri <136313166+akshatkhatri@users.noreply.github.com>
2024-01-07 01:12:41 +0530

    Create day55.cpp

---
+++ day55.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int max = 0;
+    for(int i = 0 ; i<accounts.size();  i++)
+    {   int sum = 0;
+        for(int j = 0 ; j<accounts[0].size();  j++)
+        {
+       sum = sum + accounts[i][j];
+        }
+        if(sum>max) max = sum ;
+    }
+    return max;
+    }
+};
