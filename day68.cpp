f59d58a151f689eebc11c7b84480db748b2cd298
Akshat Khatri <136313166+akshatkhatri@users.noreply.github.com>
2024-01-20 00:08:45 +0530

    Create day68.cpp

---
+++ day68.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int answer = 0;
+
+        while(x != 0){
+            int digit = x % 10;
+            if((answer > INT_MAX/10) || (answer < INT_MIN/10)){
+                return 0;
+            }
+
+            answer = answer * 10 + digit;
+            x = x / 10;
+        }
+
+        return answer;
+    }
+};
