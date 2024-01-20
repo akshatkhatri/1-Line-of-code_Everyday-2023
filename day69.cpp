92e6e64f4db00d3b26dbc591b11507f68ffed5b8
Akshat Khatri <136313166+akshatkhatri@users.noreply.github.com>
2024-01-21 02:58:35 +0530

    Create day69.cpp

---
+++ day69.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) { 
+       sort(nums.begin(),nums.end());
+        for(int i=1;i<nums.size();i+=2)
+        {
+            if(nums[i]!=nums[i-1])
+                return nums[i-1];
+        }
+        return nums[nums.size()-1];
+    }
+};
+
+// alternative 
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) { 
+       unordered_map<int,int> a;
+	   for(auto x: nums)
+		   a[x]++;
+	   for(auto z:a)
+		   if(z.second==1)
+			   return z.first;
+	   return -1;
+    }
+};
