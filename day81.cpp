e17238a435240d60421e3ac2e9e16008b5476dd2
Akshat Khatri <136313166+akshatkhatri@users.noreply.github.com>
2024-02-03 00:51:48 +0530

    Update day81.cpp

--- day81.cpp
+++ day81.cpp
@@ -1,3 +1,57 @@
+//POTD
+#include <vector>
+#include <unordered_set>
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+
+        for (int i = 0; i < 9; ++i) {
+            if (!isValidGroup(board, i, 0, i, 8)) {
+                return false;
+            }
+        }
+
+        for (int j = 0; j < 9; ++j) {
+            if (!isValidGroup(board, 0, j, 8, j)) {
+                return false;
+            }
+        }
+
+        
+        for (int i = 0; i < 9; i += 3) {
+            for (int j = 0; j < 9; j += 3) {
+                if (!isValidGroup(board, i, j, i + 2, j + 2)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;  
+    }
+
+private:
+    bool isValidGroup(const vector<vector<char>>& board, int startRow, int startCol, int endRow, int endCol) {
+        unordered_set<char> sudoku_hash;
+
+        for (int i = startRow; i <= endRow; ++i) {
+            for (int j = startCol; j <= endCol; ++j) {
+                char current = board[i][j];
+
+                if (current != '.' && sudoku_hash.count(current) > 0) {
+                    return false;
+                }
+
+                sudoku_hash.insert(current);
+            }
+        }
+
+        return true;  // The group is valid
+    }
+};
+
+
+
 /* Problem statement
 
 You have been given an array ‘a’ of ‘n’ non-negative integers.You have to check whether the given array is sorted in the non-decreasing order or not.
