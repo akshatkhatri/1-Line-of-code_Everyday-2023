fc2c18741dfc1ff331f0753dae7d4431834fc564
Akshat Khatri <jitikhatri@gmail.com>
2024-03-24 23:32:13 +0530

    day122

---
+++ day122/script.js
@@ -0,0 +1,18 @@
+const fetchPromise = fetch(
+    "bad-scheme://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
+  );
+  
+  fetchPromise
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data[0].name);
+    })
+    .catch((error) => {
+      console.error(`Could not get products: ${error}`);
+    });
+  
