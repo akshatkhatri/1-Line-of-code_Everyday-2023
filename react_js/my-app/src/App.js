67e4cc10a646931efab91dcf0b706e5e5abac639
Akshat Khatri <jitikhatri@gmail.com>
2024-04-23 23:53:29 +0530

    changes

--- react_js/my-app/src/App.js
+++ react_js/my-app/src/App.js
@@ -1,13 +1,15 @@
 // import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
 
 
 function App() {
+  const [darkmode,setDarkmode]=useState("light");
   return (
     <>
-      <Navbar title="Text-Converter" firstLink="home" secondLink="About" />
+      <Navbar title="Text-Converter" firstLink="home" secondLink="About" mode={darkmode}/>
       <div className="container">
         <Textform heading="Enter the text" />
       </div>
